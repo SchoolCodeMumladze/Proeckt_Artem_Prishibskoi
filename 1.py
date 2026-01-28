9a15a29b44557475ba7aec4c50fe9f48c3f0bfbf
Artem_Prish <artem005prish5@gmail.com>
2026-01-28 23:03:38 +0300

    улучшил чуток код

--- 1.py
+++ 1.py
@@ -1,15 +1,16 @@
 budshet = int(input('Введите ваш бюджет в руб.: '))
 nach_budshet = budshet
 kategoria_glav = []
-print('Для остановки подсчёта напишите Стоп')
+print('Для остановки подсчёта нажмите Enter')
 print("Если в категории должно быть словосочетание, пожалуйста, "
       "вместо пробела используйте символ _")
+print('Например: Проезд_автобус 100')
 schochic = 0
 
 while True:
     user_input = input('Введите категорию и стоимость через пробел: ')
 
-    if user_input.lower() == 'стоп':
+    if user_input.lower() == '':
         break
 
     kategoria, stoimost = user_input.split()
@@ -17,22 +18,22 @@ while True:
     budshet -= stoimost
     schochic += stoimost
     kategoria_glav.append((kategoria, stoimost))
+
+print("| Категория        | Сумма           |    %   |")
+print("|------------------|-----------------|--------|")
+
+for kategoria, summa in kategoria_glav:
+    percent = (summa / nach_budshet) * 100
+    print(f"| {kategoria:<16} | {summa:>10} руб. | {percent:>5.1f}% |")
+
+print("|------------------|-----------------|--------|")
+
+potracheno_percent = (schochic / nach_budshet) * 100
+print(f"| {'Потрачено':<16} | {f'{schochic} руб.':^15} | {potracheno_percent:>5.1f}% |")
+
+print("|------------------|-----------------|--------|")
+
+ostatok_percent = (budshet / nach_budshet) * 100
+print(f"| {'Осталось':<16} | {f'{budshet} руб.':^15} | {ostatok_percent:>5.1f}% |")
 if budshet < 0:
-    print(f'Привышен лимит бюджета. Не хватает {abs(budshet)} рублей')
-else:
-    print("| Категория        | Сумма           |    %   |")
-    print("|------------------|-----------------|--------|")
-
-    for kategoria, summa in kategoria_glav:
-        percent = (summa / nach_budshet) * 100
-        print(f"| {kategoria:<16} | {summa:>10} руб. | {percent:>5.1f}% |")
-
-    print("|------------------|-----------------|--------|")
-
-    potracheno_percent = (schochic / nach_budshet) * 100
-    print(f"| {'Потрачено':<16} | {f'{schochic} руб.':^15} | {potracheno_percent:>5.1f}% |")
-
-    print("|------------------|-----------------|--------|")
-
-    ostatok_percent = (budshet / nach_budshet) * 100
-    print(f"| {'Осталось':<16} | {f'{budshet} руб.':^15} | {ostatok_percent:>5.1f}% |")
+    print(f"Не хватает {abs(budshet)}руб.")
